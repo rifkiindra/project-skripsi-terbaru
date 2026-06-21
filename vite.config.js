be6802d6b317354b6f9b221076e56cd612a25e5f
mymac <mymac@mymacs-MacBook-Pro.local>
2026-06-21 23:54:36 +0700

    Fix vite build output

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
+            buildDirectory: "build",
         }),
     ],
     build: {
-        outDir: 'public/build',      // hasil build langsung ke public/build
+        outDir: "public/build", // hasil build langsung ke public/build
         emptyOutDir: true,
-        manifest: true,
-        rollupOptions: {
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-        },
     },
 });
-
